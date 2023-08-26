1b121a183c099fefcd09c436046a81d753aed38a
VictorRevers <victorrkasnowski@gmail.com>
2023-08-26 16:29:56 -0300

    sec06-2-commit

---
+++ FirstStepsInJavaWithSpringBoot/rest-with-springboot-java/src/main/java/com/victorrevers/MathController.java
@@ -0,0 +1,23 @@
+package com.victorrevers;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController //= @Controller \n @ResponseBody
+public class MathController {
+	
+	private final AtomicLong counter = new AtomicLong();
+	
+	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method=RequestMethod.GET)
+	public Double sum(
+			@PathVariable(value = "numberOne")String numberOne,
+			@PathVariable(value = "numberOne")String numberTwo
+			) {
+		return 1D;
+	}
+}
+
